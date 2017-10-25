c772e006edb609d0497ebc019269db58e34a3545
alvinchau2013@gmail.com <alvinchau2013@gmail.com>
2017-10-25 13:25:00 -0400

    test

--- client.py
+++ client.py
@@ -13,10 +13,10 @@ stop_stream = str(sys.argv[4])
 size = int(sys.argv[5])
 count = 1073741824
 
-
+#python client.py Toshtp 12345 tcp stop 1024
 if tcp_udp == 'tcp':			#TCP 
 	if stop_stream == 'stop':	#Stop
-		print "RUNNING TCP STOP AND WAIT"
+		print("RUNNING TCP STOP AND WAIT")
 		'''
 		'''
 		startmessage=' '*size
@@ -35,27 +35,57 @@ if tcp_udp == 'tcp':			#TCP
 		size=recv(1024)
 
 
-	else:#python client.py Toshtp 12345 tcp streaming 65536  #TCP stream
-		print "RUNNING TCP STREAMING"
+	else:#python client.py Toshtp 12345 tcp streaming 1024
+		print("RUNNING TCP STREAMING")
 		'''
-		send(startmsg)     		done 
-		ack, size = read()		done
-		while (count > 0)		
-    		send(buffer, size)
-    		count -= size
+		1G = 2^30
+		1M = 2^20
+		need_ack, msg_size = read_from_cmd_line() 	done 
+		startmsg = { need_ack=0, total_size=1G } 	done
+		send(startmsg) 								done
+		count = total_size done						done
+		start_time = start_timer()					done
+		while (count > 0)
+		    bytes_sent = send(buffer, msg_size)
+		    if (bytes_sent != msg_size)
+		        error()
+		    count -= bytes_sent 
+		stop_time = stop_timer()
+		print (stop_time - start_time)/1M   # throughput in megabytes per second
 		'''
 		server_sock = socket.socket(socket.AF_INET,socket.SOCK_STREAM)
 		server_sock.connect((host,port))
-		server_sock.send(str(size)) 	#send the size
-		ack = server_sock.recv(1024) 	#get the ack bit
-		count = 0
+		total_size = 1073741824
+		message =  "total_size:"+str(total_size)+",message_size:"+str(size)
+		server_sock.send(message.encode('utf-8')) 	#send the size
+		count = total_size
+		buffer_ = ' '*size
+		message_count = 0
+		total_amt_sent = 0
+		ack = server_sock.recv(1024)
+		if ack == 1:
+			print("ack. received beginning")
+		start_time = time.time()
 		while(count>0):
-			server_sock.send(' '+str())
-		server_sock.close()
+			amt_sent = server_sock.send(buffer_)
+			total_amt_sent += amt_sent
+			if(amt_sent < size):
+				print("ERROR: Amount sent was less then the buffer size")
+				count-= amt_sent
+			else:
+				count -= size
+			message_count+=1
+		stop_time = time.time()
+		print("number of messages sent:"+message_count)
+		print("number of bytes sent:"+total_amt_sent)
+		diff = stop_time - start_time
 
+		print("time took:" + diff)
+		server_sock.close()
+		
 else: 							#UDP
 	if stop_stream == 'stop':	#stop
-		print "RUNNING UDP STOP AND WAIT"
+		print("RUNNING UDP STOP AND WAIT")
 		'''
 		send(startmsg)
 		ack, size = read()
@@ -64,6 +94,6 @@ else: 							#UDP
     		count -= size
 		'''
 	else:						#UDP stream
-		print "RUNNING UDP STREAMING"
+		print("RUNNING UDP STREAMING")
 		'''
 		'''
--- server.py
+++ server.py
@@ -10,42 +10,48 @@ stop_stream = str(sys.argv[3])
 
 if tcp_udp == 'tcp':			#TCP 
 	if stop_stream == 'stop':	#Stop
-		print "RUNNING TCP STOP AND WAIT"
+		print("RUNNING TCP STOP AND WAIT")
 		'''
 		'''
 
 	else:#python server.py 12345 tcp stream #TCP stream
-		print "RUNNING TCP STREAMING"
+		print("RUNNING TCP STREAMING")
 		'''
-		startmsg, size = read()			done
-		send(ackbuf, 1)					done
-		while (buf, size = read())		
-    		count += size
-    		if (count >= expected_size)
-        		break
+		ack, size = read()
+		send(ackbuf, 1)
+		while (buf, bytes_read = read())
+		    count += bytes_read
+		    if (count >= expected_size)
+		        break
 		'''
 		server_sock = socket.socket(socket.AF_INET,socket.SOCK_STREAM) 
 		host = socket.gethostname() 			# Get local machine name
 		server_sock.bind((host, port)) 			# Bind to the port
 		server_sock.listen(5)					# Get to listen
 		client_sock, addr = server_sock.accept()# Establish connection with client
-		buff_size = int(client_sock.recv(1024))	# Get the buffer size
+		temp = client_sock.recv(1024).split(',')
+		total_size = int(temp[0].split(":")[1])
+		message_size = int(temp[1].split(":")[1])
 		client_sock.send('1')					# Send the ack bit
-		count = 0
+		byte_count = 0
+		messages = 0
 		while True:
-			data, size = client_sock.recv(buff_size)
-			count += size
-			if(count >= buff_size)
+			data = client_sock.recv(message_size)
+			byte_count += len(data)
+			messages+=1
+			if(byte_count >= total_size):
 				break
+		print("num of messages read"+messages)
+		print("num of bytes read:"+byte_count)
 
 		client_sock.close()    
 
 else: 							#UDP
 	if stop_stream == 'stop':	#stop
-		print "RUNNING UDP STOP AND WAIT"
+		print("RUNNING UDP STOP AND WAIT")
 		'''
 		'''
 	else:						#UDP stream
-		print "RUNNING UDP STREAMING"
+		print("RUNNING UDP STREAMING")
 		'''
 		'''
