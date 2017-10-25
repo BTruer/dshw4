8c0db361c967dab7d3c81dbce6ecc48d487df608
alvinchau2013@gmail.com <alvinchau2013@gmail.com>
2017-10-25 13:52:01 -0400

    worked on tcp

--- client.py
+++ client.py
@@ -11,7 +11,7 @@ port = int(sys.argv[2])
 tcp_udp = str(sys.argv[3])
 stop_stream = str(sys.argv[4])
 size = int(sys.argv[5])
-count = 1073741824
+
 
 #python client.py Toshtp 12345 tcp stop 1024
 if tcp_udp == 'tcp':			#TCP 
@@ -19,10 +19,17 @@ if tcp_udp == 'tcp':			#TCP
 		print("RUNNING TCP STOP AND WAIT")
 		'''
 		startmessage=' '*size
-
+		total_size = 1073741824
 		client_sock = socket.socket(socket.AF_INET,socket.SOCK_STREAM)
 		client_sock.connect((host,port))
-		client_sock.send(str(size))
+		message = "total_size:"+str(total_size)+",message_size:"+str(size)
+		client_sock.send(message.encode('utf-8')) 
+		count = total_size
+		buffer_ = ' '*size
+		buffer_ = buffer_.encode('utf-8')
+		message_count = 0
+		total_amt_sent = 0
+		
 		ack = client_sock.recv(1024) 
 
 		#start message sends the total amount of data to be transfered
@@ -32,11 +39,7 @@ if tcp_udp == 'tcp':			#TCP
 		
 		startmessage=recv(1024)
 		size=recv(1024)
-<<<<<<< HEAD
-
-=======
 		'''
->>>>>>> c4351f22dfa2dc9ccdb2cd5e2766a538f77ddc03
 	else:#python client.py Toshtp 12345 tcp streaming 1024
 		print("RUNNING TCP STREAMING")
 		'''
